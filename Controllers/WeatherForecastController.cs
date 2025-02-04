68e4685eb21aad560b8e61a7b94436c8be4b2667
Gustavo Oliveira <Gustavooliveirarocha@hotmail.com>
2025-02-03 21:28:59 -0300

    teste

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,7 +19,7 @@ namespace Estudos_AspNetCoreBasics.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "WeatherForecast")]
         public IActionResult Get()
         {
             return Ok( Enumerable.Range(1, 5).Select(index => new WeatherForecast
