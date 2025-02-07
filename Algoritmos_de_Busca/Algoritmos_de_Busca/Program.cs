a058c137e7b65d656b9d90578e1debf861bd3321
Gustavo Oliveira <Gustavooliveirarocha@hotmail.com>
2025-02-06 21:22:11 -0300

    Implementando Teste e BenchMark

--- Algoritmos_de_Busca/Algoritmos_de_Busca/Program.cs
+++ Algoritmos_de_Busca/Algoritmos_de_Busca/Program.cs
@@ -1,30 +1,65 @@
-﻿using System.ComponentModel;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.ComponentModel;
 
-namespace Algoritmos_de_Busca
+namespace Algoritmos_de_Busca;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Implementação de Algoritmos de Busca Linear!");
+        Console.WriteLine("Implementação de Algoritmos de Busca Linear!");
 
-            var lista = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            var valorBuscado = 8;
+       // Console.WriteLine($"Elemento encontrado no indice {BuscaLinear(lista, valorBuscado)}, com lista de tamanho {lista.Count}");
+        BenchmarkRunner.Run<Buscas>();
+    }
+
+    
+}
+
+public class Buscas
+{
+    private List<int> lista;
+    private int valorBuscado = 1000;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        // Gera 100_000 números ordenados para teste
+         lista = Enumerable.Range(1, 1000).ToList();
         
-            BuscaLinear(lista, valorBuscado);
-            Console.WriteLine($"Elemento encontrado no indice {BuscaLinear(lista, valorBuscado)}, com lista de tamanho {lista.Count}");
+    }
 
-        }
-
-        private static int? BuscaLinear(List<int> lista, int valorBuscado)
+    [Benchmark]
+    public int? BuscaLinear()
+    {
+        for (int i = 0; i < lista.Count; i++)
         {
-            for (int i = 0; i < lista.Count; i++)
-            {
-                if (lista[i] == valorBuscado)
-                    return i;
-            }
-            return null;
+            if (lista[i] == valorBuscado)
+                return i;
         }
+        return null;
+    }
+
+    [Benchmark]
+    public  int? BuscaBinaria()
+    {
+        //lista = lista.Order().ToList();
+        var IndiceInicioLista = 0;
+        var indiceFimLista = lista.Count - 1;
+        while (IndiceInicioLista <= indiceFimLista)
+        {
+            var indiceMeioLista = (IndiceInicioLista + indiceFimLista) / 2;
+
+            if (lista[indiceMeioLista] == valorBuscado)
+                return indiceMeioLista;
+
+            if (lista[indiceMeioLista] < valorBuscado)
+                IndiceInicioLista = indiceMeioLista + 1;
+            else
+                indiceFimLista = indiceMeioLista - 1;
+        }
+        return null;
     }
 }
---
+++ Algoritmos_de_Busca/Busca_Linear/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Busca_Linear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementação de Algoritmos de Busca Linear!");
+
+            var lista = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var valorBuscado = 8;
+
+            BuscaLinear(lista, valorBuscado);
+            Console.WriteLine($"Elemento encontrado no indice {BuscaLinear(lista, valorBuscado)}, com lista de tamanho {lista.Count}");
+
+        }
+
+        private static int? BuscaLinear(List<int> lista, int valorBuscado)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i] == valorBuscado)
+                    return i;
+            }
+            return null;
+        }
+    }
+}
