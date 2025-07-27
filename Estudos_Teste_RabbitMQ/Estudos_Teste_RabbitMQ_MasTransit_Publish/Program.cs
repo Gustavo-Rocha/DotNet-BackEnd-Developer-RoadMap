41de9ac30f3ad4d06c718c6a178136685f3ddec3
Gustavo Oliveira <Gustavooliveirarocha@hotmail.com>
2025-07-27 19:18:33 -0300

    ajuste

---
+++ Estudos_Teste_RabbitMQ/Estudos_Teste_RabbitMQ_MasTransit_Publish/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Estudos_Teste_RabbitMQ_MasTransit_Publish
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Estudos_Teste_RabbitMQ/MassTransit_Estudos_Teste_RabbitMQ_Consumer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MassTransit_Estudos_Teste_RabbitMQ_Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
